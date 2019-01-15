107c8d8c99f068566cf6b75ea36b3770ef148079
Alan Ronald <alanronald55@gmail.com>
2019-01-15 22:51:15 +0530

    Create checkamstrong.py

---
+++ checkamstrong.py
@@ -0,0 +1,13 @@
+num=int(raw_input())
+
+order=len(str(num))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum+= digit**order
+    temp//=10
+if num==sum:
+    print "yes"
+else:
+    print "no"
