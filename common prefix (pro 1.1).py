b651b9f2de88914c2553288677e7aff329c6804e
Alan Ronald <alanronald55@gmail.com>
2019-06-19 04:49:18 +0530

    Update and rename common prefix (pro 1.1) to common prefix (pro 1.1).py

--- common prefix (pro 1.1).py
+++ common prefix (pro 1.1).py
@@ -1,10 +1,10 @@
 n=int(input())
-g=[]
+a=[]
 for k in range(0,n):  
     d=input()
-    g.append(d)
+    a.append(d)
 list=[]
-for k in zip(*g):
+for k in zip(*a):
     if (k.count(k[0])==len(k)): 
         list.append(k[0])
     else:
